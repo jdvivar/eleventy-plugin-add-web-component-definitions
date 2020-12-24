cdd4a50264fcfc3dffb2bf85c64b698e2e47c21f
Doug Baldwin <gdouglasbaldwin@protonmail.com>
2020-12-24 10:27:41 +0800

    Configure and test options

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const AddWebComponentDefinitions = require( "./add-web-component-definitions" );
 
 module.exports = {
-  configFunction: function (eleventyConfig) {
-    eleventyConfig.addTransform('add-web-component-definitions', AddWebComponentDefinitions);
+  configFunction: function (eleventyConfig, options = {}) {
+    eleventyConfig.addTransform(
+      'add-web-component-definitions',
+      AddWebComponentDefinitions.bind(null, options)
+    );
   }
 }
--- test/add-web-component-definitions.test.js
+++ test/add-web-component-definitions.test.js
@@ -3,7 +3,7 @@ const addWebComponentDefinitions = require('../add-web-component-definitions')
 
 test('Not html', t => {
   t.is(
-    addWebComponentDefinitions(
+    addWebComponentDefinitions.bind(null, {})(
       'asdf',
       ''
     ),
@@ -13,7 +13,7 @@ test('Not html', t => {
 
 test('Is html', t => {
   t.is(
-    addWebComponentDefinitions(
+    addWebComponentDefinitions.bind(null, {})(
       '<html><head></head><body>asdf</body></html>',
       'index.html'
     ),
@@ -23,7 +23,7 @@ test('Is html', t => {
 
 test('h1 tag', t => {
   t.is(
-    addWebComponentDefinitions(
+    addWebComponentDefinitions.bind(null, {})(
       '<html><head></head><body><h1>asdf</h1></body></html>',
       'index.html'
     ),
@@ -33,7 +33,47 @@ test('h1 tag', t => {
 
 test('Custom tag', t => {
   t.is(
-    addWebComponentDefinitions(
+    addWebComponentDefinitions.bind(null, {})(
+      '<html><head></head><body><custom-tag>asdf</custom-tag></body></html>',
+      'index.html'
+    ),
+    '<html><head></head><body><custom-tag>asdf</custom-tag><script type="module" src="/js/components/custom-tag/custom-tag.js"></script></body></html>'
+  )
+})
+
+test('Configure path', t => {
+  t.is(
+    addWebComponentDefinitions.bind(null, { path: tag => `/test/${tag}.js` })(
+      '<html><head></head><body><custom-tag>asdf</custom-tag></body></html>',
+      'index.html'
+    ),
+    '<html><head></head><body><custom-tag>asdf</custom-tag><script type="module" src="/test/custom-tag.js"></script></body></html>'
+  )
+})
+
+test('Configure position', t => {
+  t.is(
+    addWebComponentDefinitions.bind(null, { position: 'beforebegin' })(
+      '<html><head></head><body><custom-tag>asdf</custom-tag></body></html>',
+      'index.html'
+    ),
+    '<html><head></head><script type="module" src="/js/components/custom-tag/custom-tag.js"></script><body><custom-tag>asdf</custom-tag></body></html>'
+  )
+})
+
+test('Configure verbose', t => {
+  t.is(
+    addWebComponentDefinitions.bind(null, { verbose: true })(
+      '<html><head></head><body><custom-tag>asdf</custom-tag></body></html>',
+      'index.html'
+    ),
+    '<html><head></head><body><custom-tag>asdf</custom-tag><script type="module" src="/js/components/custom-tag/custom-tag.js"></script></body></html>'
+  )
+})
+
+test('Configure silent', t => {
+  t.is(
+    addWebComponentDefinitions.bind(null, { silent: true })(
       '<html><head></head><body><custom-tag>asdf</custom-tag></body></html>',
       'index.html'
     ),
