91764e145f3f41b3c364c6efbe2cb4f27a62b608
Doug Baldwin <gdouglasbaldwin@protonmail.com>
2020-12-24 18:22:37 +0800

    feat: add specifiers to configuration object

--- add-web-component-definitions.js
+++ add-web-component-definitions.js
@@ -5,12 +5,13 @@ const h = require('hastscript')
 const toHtml = require('hast-util-to-html')
 
 const isWebComponent = (tag) => tag && (/(\w+(-\w+)+)/g).test(tag)
-const logName = '[add-web-component-definitions]';
+const logName = '[add-web-component-definitions]'
 
 module.exports = function (options, content, outputPath) {
   if (outputPath.endsWith('.html')) {
     options = Object.assign(
       {
+        specifier: {},
         path: function (tag) { return `/js/components/${tag}/${tag}.js` },
         position: 'beforeend',
         verbose: false,
@@ -20,6 +21,7 @@ module.exports = function (options, content, outputPath) {
 
     if (options.verbose) {
       console.log(logName, 'Examining', outputPath)
+      console.log(logName, 'options', options)
     }
 
     const tags = new Set()
@@ -41,20 +43,32 @@ module.exports = function (options, content, outputPath) {
     }
 
     if (tags.size) {
-      const value = [...tags]
-        .map(tag => `import "${options.path(tag)}";`)
-        .map(v => {
-          if (!options.quiet) {
-            console.log(logName, v)
-          }
-          return v
+      const value = [...new Set(
+        [...tags]
+        .map(tag => {
+          const path = options.specifier[tag] || options.path
+          const typeOfPath = typeof path
+          console.assert(
+            'string function'.split(' ').includes(typeOfPath),
+            { tag: tag, path: path, errorMsg: 'specifier/path must be a string or a function' }
+          )
+          const v = typeOfPath === 'string' ? path : path(tag);
+          return v ? `import "${v}";` : null
         })
-        .join('\n')
+      )]
+      .filter(Boolean)
+      .join('\n')
+      
+      if (!options.quiet) {
+        value.split('\n').forEach(v => console.log(logName, v) )
+      }
+
+      const child = h('script', { type: 'module' }, [{ type: 'text', value }])
 
       if (options.position === 'afterbegin') {
-        body.children.unshift(h('script', { type: 'module' }, [{ type: 'text', value }]))
+        body.children.unshift(child)
       } else {
-        body.children.push(h('script', { type: 'module' }, [{ type: 'text', value }]))
+        body.children.push(child)
       }
     }
 
